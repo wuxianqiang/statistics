d6ca68c96d3d4c7dcc453754a5030b825b781d48
wuxianqiang <2631640352@qq.com>
2022-06-18 20:36:28 +0800

    init

---
+++ src/components/ResultList.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import type { FileChangeInfo } from 'fs/promises';
+
+const handleChange = (e: any) => {
+  const file = e.target.files[0]
+}
+</script>
+
+<template>
+  <div>
+    <input type="file" name="execl" id="myfile" @change="handleChange">
+  </div>
+</template>
+
+<style scoped>
+
+</style>
