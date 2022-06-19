ad87a958ee1dc849c739dc614ac017cd764ac3a5
wuxianqiang <2631640352@qq.com>
2022-06-19 21:25:21 +0800

    change config

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import ElementPlus from 'unplugin-element-plus/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://github.com/wuxianqiang/statistics/',
+  base: 'https://wuxianqiang.github.io/statistics/',
   plugins: [vue(), ElementPlus()],
   resolve: {
     alias: {
